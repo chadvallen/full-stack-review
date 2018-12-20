e5f73fdb4b4be939848f4dd64a484ce89749cb4c
Chad Allen <chadvallen@gmail.com>
2018-12-20 13:38:35 -0700

    Finished full stack review

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,8 +1,24 @@
 const INITIAL_STATE = {
-    user: null,
+    user: null
 
 };
 
-function reducer(state=INITIAL_STATE, action) {
-    
+const USER_LOGIN = 'USER_LOGIN';
+
+export function userLogin(user) {
+    return {
+        type: USER_LOGIN,
+        payload: user
+    }
 }
+
+function reducer(state=INITIAL_STATE, action) {
+    switch(action.type) {
+        case USER_LOGIN:
+            return {...state, user: action.payload};
+        default:
+            return state;
+    }
+}
+
+export default reducer;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './reset.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as BR } from 'react-router-dom'; 
+import { BrowserRouter as BR } from 'react-router-dom';
+import { Provider} from 'react-redux'; 
+import store from './ducks/store';
 
 ReactDOM.render(
-<BR>
-    <App />
+<BR>   
+    <Provider store={store}>
+        <App />
+    </Provider>
 </BR>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
